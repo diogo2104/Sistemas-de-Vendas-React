fd41f27a57615c3221dc698d95717e63df2866e5
Diogo Alessandro de Paula <112518794+diogo2104@users.noreply.github.com>
2026-08-06 20:00:40 -0300

    Update categoriaService.ts

--- frontend/src/app/services/categoriaService.ts
+++ frontend/src/app/services/categoriaService.ts
@@ -3,5 +3,5 @@ import { API_BASE_URL } from './api';
 export async function listarCategorias() {
   const response = await fetch(`${API_BASE_URL}/categorias/index.php`);
   if (!response.ok) throw new Error('Erro ao listar categorias');
-  return response.json();
+  return response.json(); 
 }
